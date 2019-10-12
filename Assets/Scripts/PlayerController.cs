3717be864d67cbd4a413cc5bab679b391daf1b04
Joshua Quinones <jiq4449@ad.rit.edu>
2019-10-12 16:06:20 -0400

    Finishing character movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,14 +17,19 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 forwardAdjustment = speed*Input.GetAxis("Vertical") * transform.forward;
         Vector3 rightAdjustment = speed*Input.GetAxis("Horizontal") * transform.right;
+        //Determines fall time
         gravity += gravSpeed * Time.deltaTime;
+
+        //Sets the gravity to the jump speed if the user preseses spacebar
         if (Input.GetKeyDown(KeyCode.Space)&&Character.isGrounded)
         {
             gravity = jumpSpeed;
         }
+        //Makes the user go up if their gravity is positive
         Vector3 upAdjustment = gravity * transform.up;
-        Vector3 move = forwardAdjustment + rightAdjustment + upAdjustment;
 
+        //Enables movement as a result of the following attributes
+        Vector3 move = forwardAdjustment + rightAdjustment + upAdjustment;
         Character.Move(move*Time.deltaTime);
 
 
