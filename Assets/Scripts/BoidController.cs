5e2aec43824a89662ceb04b907757093fe158b6e
crazyjackel <47047592+crazyjackel@users.noreply.github.com>
2019-10-12 19:43:56 -0400

    boids

---
+++ Assets/Scripts/BoidController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidController : MonoBehaviour
+{
+    public Vector3 Direction;
+
+    public Vector3 start = Vector3.forward*300;
+    public Vector3 end = Vector3.back*300;
+
+    public static void atBounds(Boids boid, BoidController boidController)
+    {
+        if (boidController.end.z > boid.transform.position.z)
+        {
+            boid.transform.position = new Vector3(boid.transform.position.x,boid.transform.position.y,boidController.start.z);
+        }
+        if (boidController.start.z < boid.transform.position.z)
+        {
+            boid.transform.position = new Vector3(boid.transform.position.x, boid.transform.position.y, boidController.end.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Boids.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boids : MonoBehaviour
+{
+
+    public float speed;
+    public BoidController flockDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed += Random.Range(-3,3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position,20);
+        Vector3 avoidDirection = Vector3.zero;
+        foreach(Collider c in colliders)
+        {
+            if(c.gameObject.GetComponent<Boids>() != null)
+            {
+                Vector3 dif = (this.transform.position-c.transform.position);
+                dif = dif / (1+dif.magnitude * dif.magnitude);
+                avoidDirection += dif;
+            }
+            if(c.gameObject.GetComponent<PlayerController>()!= null)
+            {
+                Vector3 dif = (this.transform.position - c.transform.position);
+                dif = dif / (1+dif.magnitude * dif.magnitude);
+                avoidDirection += 10*dif;
+            }
+        }
+        avoidDirection = avoidDirection.normalized + flockDirection.Direction;
+        avoidDirection = avoidDirection.normalized;
+        avoidDirection = new Vector3(avoidDirection.x, 0, avoidDirection.z);
+        transform.position += avoidDirection * Time.deltaTime*speed;
+
+        BoidController.atBounds(this, flockDirection);
+    }
+}
