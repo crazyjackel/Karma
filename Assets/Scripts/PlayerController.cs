d70f6180b01493f2ff73958535100d526d440c4d
Joshua Quinones <jiq4449@ad.rit.edu>
2019-10-12 15:00:47 -0400

    Worked on movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float speed = 200;
+    float speed = 20;
+    float jumpSpeed = 50;
     CharacterController Character;
     void Start()
     {
@@ -13,10 +14,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 forwardAdjustment = speed*Input.GetAxis("Vertical") * DataManager.INSTANCE.MainPlayerCamera.transform.forward;
-        Vector3 RightAdjustment = speed*Input.GetAxis("Horizontal") * transform.right;
-        Vector3 move = forwardAdjustment + RightAdjustment;
-        Character.SimpleMove(move*Time.deltaTime);
+        Vector3 forwardAdjustment = speed*Input.GetAxis("Vertical") * transform.forward;
+        Vector3 rightAdjustment = speed*Input.GetAxis("Horizontal") * transform.right;
+        Vector3 move = forwardAdjustment + rightAdjustment;
+        Character.Move(move*Time.deltaTime);
+
+        Vector3 jump = jumpSpeed*Input.GetAxis("Jump") * transform.up;
 
     }
 }
