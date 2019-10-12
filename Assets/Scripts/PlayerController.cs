b7827635cb9ac0a10437ad5cfaaa8fdaf2cdf1a7
crazyjackel <47047592+crazyjackel@users.noreply.github.com>
2019-10-12 14:38:09 -0400

    Character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float speed = 200;
+    CharacterController Character;
     void Start()
     {
-        
+        Character = GetComponent<CharacterController>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 forwardAdjustment = speed*Input.GetAxis("Vertical") * DataManager.INSTANCE.MainPlayerCamera.transform.forward;
+        Vector3 RightAdjustment = speed*Input.GetAxis("Horizontal") * transform.right;
+        Vector3 move = forwardAdjustment + RightAdjustment;
+        Character.SimpleMove(move*Time.deltaTime);
+
     }
 }
