f1b5211be76bd55c99d8db69e490643d8e11d967
crazyjackel <47047592+crazyjackel@users.noreply.github.com>
2019-10-13 14:19:10 -0400

    Opening

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InteractableObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public UnityEvent[] interactEvent;
 
-    // Update is called once per frame
-    void Update()
+    public void Interact()
     {
-        
+        foreach(UnityEvent unityEvent in interactEvent)
+        {
+            unityEvent.Invoke();
+        }
     }
 }
